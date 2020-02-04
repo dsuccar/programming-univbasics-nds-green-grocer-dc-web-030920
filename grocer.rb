9538c002e9a4b9e189d1d449298e1f7e8372a663
Daniel Succar <danielsuccar@icloud.com>
2020-02-04 16:55:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -76,6 +76,7 @@ while i<coupons.length
   end
   i += 1
 end
+cart
 end
   #cart     [{:item=>"AVOCADO", :price=>3.0, :clearance=>true, :count=>2}]
   #coupons  [{:item=>"AVOCADO", :num=>2, :cost=>5.0}]
