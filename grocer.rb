d93fd798f68441ea15ad01fc5e350cf71a000bae
Daniel Succar <danielsuccar@icloud.com>
2020-02-04 16:54:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -57,11 +57,20 @@ while i<coupons.length
   cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
   couponed_item_name = "#{coupons[i][:item]} W/COUPON"
   cart_item_with_coupon = cart_item = find_item_by_name_in_collection(couponed_item_name, cart)
-  if cart_item &&
-     cart_item[:count] >= coupons[i][:num]
-    if
-
-
+  if cart_item &&  cart_item[:count] >= coupons[i][:num]
+    if cart_item_with_coupon
+      cart_item_with_coupon[:count] += coupons[i][:num ]
+      cart_item -= coupons[i][:num]
+    else
+      cart_item_with_coupon = {
+        [{:item=>couponed_item_name,
+          :price=>coupons[i][:cost]/ coupons[i][:num]],
+          :clearance=>cart_item[:clearance],
+          :count=>coupons[i][:num]}]
+      }
+      cart << cart_item_with_coupon
+      cart_item[:count] -= coupons[i][:num]
+    
     end
 
   end
